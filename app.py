4c322dd968915267ac18a55cd371c31a33653f27
Ricky <rickywolf28@yahoo.com>
2019-09-18 14:39:12 -0700

    db setup

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ playlists = db.playlists
 
 app = Flask(__name__)
 
+import os
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(client = MongoClient(host=f'{host}?retryWrites=false'))
+db = client.get_default_database()
+playlists = db.playlists
 
 @app.route('/')
 def playlists_index():
