ba88df85d8d64049390088562b0b7c5a5e1a3901
Ricky <rickywolf28@yahoo.com>
2019-09-20 17:17:16 -0700

    test deploy

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ playlists = db.playlists
 
 app = Flask(__name__)
 
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird', 'videos':'cats' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!','videos':'old' }
-]
+
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
